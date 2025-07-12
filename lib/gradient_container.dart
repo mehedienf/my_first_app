276e15742614ddf99523f7e684406a0f2ab70e71
mehedienf <enammehedi06@gmail.com>
2025-07-12 21:24:03 +0600

    adding const insted of var when i stay in unchanged the value of the variable further

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_app/style_text.dart';
 
-var startAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -10,10 +10,10 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             // color: Colors.blue,
             gradient: LinearGradient(
-              colors: const [
+              colors: [
                 Color.fromARGB(255, 0, 128, 0),
                 Color.fromARGB(255, 70, 227, 70),
               ],
